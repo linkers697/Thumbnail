138213fce88abaf23cd069ca3049e5c2d468d20c
linkers697 <music3fhhgyhfgh@gmail.com>
2025-10-12 09:48:10 +0530

    Update thumbnails.py

--- AloneMusic/utils/thumbnails.py
+++ AloneMusic/utils/thumbnails.py
@@ -1,3 +1,22 @@
-# Dummy thumbnail function (Heroku safe)
+import logging
+log = logging.getLogger(__name__)
+
+# Dummy no-op function
+def _noop(*args, **kwargs):
+    return None
+
+# All thumbnail functions replaced with no-op
+generate_thumbnail = _noop
+create_thumbnail = _noop
+make_thumbnail = _noop
+get_thumbnail = _noop
+build_thumbnail = _noop
+generate_cover = _noop
+get_cover = _noop
+
+# ✅ CRUCIAL: this fixes call.py import error
 def get_thumb(*args, **kwargs):
     return None
+
+def thumbnails_enabled():
+    return False
