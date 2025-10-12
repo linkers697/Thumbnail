429b33c2e16cb4ef4f74823b9c833a57ec32d485
linkers697 <music3fhhgyhfgh@gmail.com>
2025-10-12 09:51:19 +0530

    Update thumbnails.py

--- AloneMusic/utils/thumbnails.py
+++ AloneMusic/utils/thumbnails.py
@@ -1,10 +1,12 @@
 import logging
 log = logging.getLogger(__name__)
 
+# -----------------------------
 # Dummy no-op function
 def _noop(*args, **kwargs):
     return None
 
+# -----------------------------
 # All thumbnail functions replaced with no-op
 generate_thumbnail = _noop
 create_thumbnail = _noop
@@ -14,9 +16,10 @@ build_thumbnail = _noop
 generate_cover = _noop
 get_cover = _noop
 
-# ✅ CRUCIAL: this fixes call.py import error
+# ✅ This fixes call.py import error
 def get_thumb(*args, **kwargs):
     return None
 
+# -----------------------------
 def thumbnails_enabled():
     return False
